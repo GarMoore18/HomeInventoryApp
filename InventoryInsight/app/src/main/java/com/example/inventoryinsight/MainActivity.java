e1ee3e1aacc4a2b50857d33436ebcf40fc2ea3f9
GarMoore18 <62194724+GarMoore18@users.noreply.github.com>
2021-01-19 22:19:18 -0500

    Nav work session
    Added real navigation; better theme; better navigation bar format

--- InventoryInsight/app/src/main/java/com/example/inventoryinsight/MainActivity.java
+++ InventoryInsight/app/src/main/java/com/example/inventoryinsight/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.inventoryinsight;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting up the navigation for the bottom navigation bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.home_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            //Will navigate to correct activity based on selected destination
+            if (itemId == R.id.menu_add) {
+                Toast.makeText(MainActivity.this, "ADDING", Toast.LENGTH_SHORT).show();
+                Intent intent_add = new Intent(getBaseContext(), AddActivity.class);
+                startActivity(intent_add);
+            } else if (itemId == R.id.menu_remove) {
+                Toast.makeText(MainActivity.this, "REMOVING", Toast.LENGTH_SHORT).show();
+                Intent intent_remove = new Intent(getBaseContext(), RemoveActivity.class);
+                startActivity(intent_remove);
+            } else if (itemId == R.id.menu_search) {
+                Toast.makeText(MainActivity.this, "SEARCHING", Toast.LENGTH_SHORT).show();
+                Intent intent_search = new Intent(getBaseContext(), SearchActivity.class);
+                startActivity(intent_search);
+            } else if (itemId == R.id.menu_settings) {
+                Toast.makeText(MainActivity.this, "SETTINGS", Toast.LENGTH_SHORT).show();
+                Intent intent_settings = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(intent_settings);
+            } else if (itemId == R.id.menu_contact) {
+                Toast.makeText(MainActivity.this, "CONTACTING", Toast.LENGTH_SHORT).show();
+                Intent intent_contact = new Intent(getBaseContext(), ContactActivity.class);
+                startActivity(intent_contact);
+            }
+            return true;
+        });
     }
 }
