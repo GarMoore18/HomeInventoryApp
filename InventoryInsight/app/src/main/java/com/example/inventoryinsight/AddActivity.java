9f77b8e6bc6f56d85bfde4505014f99403e67113
GarMoore18 <62194724+GarMoore18@users.noreply.github.com>
2021-01-25 18:20:01 -0500

    add screen layout
    there are now the correct fields for the add screen and an add button

--- InventoryInsight/app/src/main/java/com/example/inventoryinsight/AddActivity.java
+++ InventoryInsight/app/src/main/java/com/example/inventoryinsight/AddActivity.java
@@ -3,6 +3,8 @@ package com.example.inventoryinsight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        EditText barcodeET = findViewById(R.id.upc_field);
+        EditText nameET = findViewById(R.id.item_name_field);
+        EditText quantityET = findViewById(R.id.quantity_field);
+        EditText locationET = findViewById(R.id.location_field);
+
     }
 }
